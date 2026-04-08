3e31fd99f6436f44c6912a986b590c392de55cd4
pluto7073 <plutonia.electra.sagittaria@proton.me>
2026-04-08 16:29:03 -0600

    Duration changes

--- src/main/java/ml/pluto7073/bartending/foundations/config/BartendingCommonConfig.java
+++ src/main/java/ml/pluto7073/bartending/foundations/config/BartendingCommonConfig.java
@@ -2,6 +2,7 @@ package ml.pluto7073.bartending.foundations.config;
 
 import ml.pluto7073.bartending.TheArtOfBartending;
 import ml.pluto7073.plutonium.annotations.BooleanOption;
+import ml.pluto7073.plutonium.annotations.DoubleOption;
 import ml.pluto7073.plutonium.annotations.IntOption;
 import ml.pluto7073.plutonium.config.ServerConfig;
 import ml.pluto7073.plutonium.config.ServerConfigType;
@@ -12,7 +13,8 @@ public class BartendingCommonConfig extends ServerConfig {
     public static final BartendingCommonConfig INSTANCE = new BartendingCommonConfig(TheArtOfBartending.CONFIG_TYPE, false);
 
     @BooleanOption(defaultVal = true, hasTooltip = false) public boolean doBlackout;
-    @IntOption(defaultVal = 24000) public int yearLengthTicks;
+    @IntOption(defaultVal = 72000) public int yearLengthTicks;
+    @DoubleOption(defaultVal = 1.0) public double fermentationTimeScale;
 
     public BartendingCommonConfig(ServerConfigType<?> type, boolean copy) {
         super("bartending", TheArtOfBartending.LOGGER, type, copy);
